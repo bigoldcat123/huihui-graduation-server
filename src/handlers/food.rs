941cb68b3b4b9ec6b0814beae9de73374c34ec21
Dale Lowe <dadigua@Dales-MacBook-Pro.local>
2026-02-11 23:21:48 +0800

    Delete api.md and refresh docs

--- src/handlers/food.rs
+++ src/handlers/food.rs
@@ -1,9 +1,9 @@
-use faithea::{data::Json, post};
+use faithea::{data::{Json, inbound::FromRequest}, post};
 
-use crate::{model::{ApiResponse, input::SuggestionInput}, service};
+use crate::{model::{ApiResponse, input::SuggestionInput}, service::{self, auth::CurrentUserId}};
 
 #[post("/consecutiveSuggest")]
-async fn consecutive_suggest(food_ids: Json<SuggestionInput>) {
+async fn consecutive_suggest(food_ids: Json<SuggestionInput>, _token: FromRequest<CurrentUserId>) {
     let res: ApiResponse<_> = service::food::consecutive_suggest(food_ids.0).await.into();
     res.json()
 }
